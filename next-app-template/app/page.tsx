0a2d776f5f5f0b3d68f120856e787c82dc97e82d
Aaron Varga <aaronvarga@myyahoo.com>
2024-11-02 22:55:34 +0000

    page update

--- next-app-template/app/page.tsx
+++ next-app-template/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Building applications never looked so&nbsp;</span>
+        <span className={title()}>Building apps never looked so&nbsp;</span>
         <span className={title({ color: "violet" })}>beautiful</span>
         <div className={subtitle({ class: "mt-4" })}>
           Beautiful. Fast. Modern.
