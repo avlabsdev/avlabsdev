7c7411df45a6ffb40fa56af8c54024f60dee836c
Aaron Varga <aaronvarga@myyahoo.com>
2024-11-03 03:14:47 +0000

    restart yet again

---
+++ my-app/src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import "@fontsource/poppins"; // Defaults to weight 400
+import "@fontsource/poppins/100.css"; // Specify weight
+import "@fontsource/poppins/100-italic.css"; // Specify weight and style
+import "@fontsource/poppins/200.css"; // Specify weight
+import "@fontsource/poppins/200-italic.css"; // Specify weight and style
+import "@fontsource/poppins/300.css"; // Specify weight
+import "@fontsource/poppins/300-italic.css"; // Specify weight and style
+import "@fontsource/poppins/400.css"; // Specify weight
+import "@fontsource/poppins/400-italic.css"; // Specify weight and style
+import "@fontsource/poppins/500.css"; // Specify weight
+import "@fontsource/poppins/500-italic.css"; // Specify weight and style
+import "@fontsource/poppins/600.css"; // Specify weight
+import "@fontsource/poppins/600-italic.css"; // Specify weight and style
+import "@fontsource/poppins/700.css"; // Specify weight
+import "@fontsource/poppins/700-italic.css"; // Specify weight and style
+import "@fontsource/poppins/800.css"; // Specify weight
+import "@fontsource/poppins/800-italic.css"; // Specify weight and style
+import "@fontsource/poppins/900.css"; // Specify weight
+import "@fontsource/poppins/900-italic.css"; // Specify weight and style
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
