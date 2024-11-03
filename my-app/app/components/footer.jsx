459e5c4aac9ffd06621802445ce97ccb54826d47
Aaron Varga <aaronvarga@myyahoo.com>
2024-11-03 22:53:52 +0000

    footer updates

--- my-app/app/components/footer.jsx
+++ my-app/app/components/footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <footer className="flex p-4 items-center justify-between">
           <p>&copy; 2024 <a className="opacity-75 hover:opacity-100" href="#/">AV Labs</a>. All rights reserved.</p>
           <Link className="opacity-75 hover:opacity-100" href="#/">
-            <Image src={githubIcon} alt="GitHub Icon" width={24} height={24} />
+            <Image src={githubIcon} alt="GitHub Icon" width={20} height={20} />
           </Link>
         </footer>
       </>
