790532b57c03ec01cd9f002c315cebf801beb9f9
Aaron Varga <aaronvarga@myyahoo.com>
2024-11-03 22:42:33 +0000

    assets and comp updates

--- my-app/app/components/footer.jsx
+++ my-app/app/components/footer.jsx
@@ -1,10 +1,15 @@
-// import Image from "next/image";
+import Image from "next/image";
+import Link from "next/link";
+import githubIcon from "../../public/github.svg";
 
 export default function Footer() {
     return (
       <>
-        <footer className="flex p-4">
-          Footer
+        <footer className="flex p-4 items-center justify-between">
+          <p>&copy; 2024 <a className="opacity-75 hover:opacity-100" href="#/">AV Labs</a>. All rights reserved.</p>
+          <Link className="opacity-75 hover:opacity-100" href="#/">
+            <Image src={githubIcon} alt="GitHub Icon" width={24} height={24} />
+          </Link>
         </footer>
       </>
     );
--- my-app/app/components/headernav.jsx
+++ my-app/app/components/headernav.jsx
@@ -1,16 +1,19 @@
-// import Image from "next/image";
+import Image from "next/image";
 import Link from "next/link";
+import flaskIcon from "../../public/flask.svg";
 
 export default function HeaderNav() {
   return (
     <>
       <header className="flex items-center justify-between p-2">
-        <Link className="p-2" href="/">AV Labs</Link>
+        <Link className="p-2 opacity-75 hover:opacity-100 flex items-center gap-2 font-medium text-lg" href="/">
+        <Image src={flaskIcon} alt="Flask Icon" width={20} height={20} />
+        AV Labs</Link>
         <nav className="flex items-center gap-8">
-          <Link className="p-2" href="/">Home</Link>
-          <Link className="p-2" href="/projects">Projects</Link>
-          <Link className="p-2" href="/about">About</Link>
-          <Link className="p-2" href="/contact">Contact</Link>
+          <Link className="p-2 opacity-75 hover:opacity-100" href="/">Home</Link>
+          <Link className="p-2 opacity-75 hover:opacity-100" href="/projects">Projects</Link>
+          <Link className="p-2 opacity-75 hover:opacity-100" href="/about">About</Link>
+          <Link className="p-2 opacity-75 hover:opacity-100" href="/contact">Contact</Link>
         </nav>
       </header>
     </>
