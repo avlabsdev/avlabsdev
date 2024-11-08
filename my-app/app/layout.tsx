3d3173e2b91d42ab46ca80bd0ffe332d730dab92
Aaron Varga <aaronvarga@myyahoo.com>
2024-11-07 23:54:25 -0500

    speed insights, vercel

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Script from "next/script";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 
@@ -46,6 +47,7 @@ export default function RootLayout({
         <main className="flex-grow h-full">
           {children}
           <Analytics />
+          <SpeedInsights />
         </main>
         <Footer />
       </body>
